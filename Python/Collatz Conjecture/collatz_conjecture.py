8d04794244b0b04ee102059e98913d7fdbafd47c
Pabasara-Prasanjith <118659065+Pabasara-Prasanjith@users.noreply.github.com>
2026-04-25 15:31:26 +0530

    Implement Collatz conjecture step calculator

---
+++ Python/Collatz Conjecture/collatz_conjecture.py
@@ -0,0 +1,14 @@
+"""collatz conjecture step calculator"""
+def steps(number):
+    """main logic is here"""
+    step_counter = 0
+    if number <= 0:
+        # example when argument is zero or a negative integer
+        raise ValueError("Only positive integers are allowed")
+    while number != 1:
+        step_counter += 1
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = number * 3 + 1
+    return step_counter
